77f60a13fa799338b6a822ee2635e935a0387363
PieroVicenti <p.vicenti@outlook.com>
2022-07-18 22:52:16 +0100

    CarsList component added

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import CarItem from './components/CarItem';
+import CarsList from './components/CarsList';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem name={"Model X"} tagline={"Starting from £59,000"} taglineCTA={"Touchless Delivery"} image={require('./assets/images/ModelX.jpeg')} />
+      <CarsList  />
       <StatusBar style="auto" />
     </View>
   );
